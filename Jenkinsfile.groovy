0abd0a0da462341fafcb73e4cf2e442f69d723d5
Sandesh Wani <sandeshwani@gmail.com>
2018-04-21 20:24:01 -0400

    checking for .groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,15 @@
+pipeline {
+    agent any
+    stages{
+        stage('Startup'){
+            steps{
+            echo 'Hello world'
+            }
+        }
+        stage('Second'){
+                    steps{
+                    echo 'Hello world'
+                    }
+        }
+    }
+}
